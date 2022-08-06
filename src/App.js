a58078187d5aaa3233173e97c27ccc91745b24c3
Izhal-Brown <informatika749@gmail.com>
2022-08-06 12:44:36 +0800

    test penambahan paragraf

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Halo nama Saya Rizhal
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
