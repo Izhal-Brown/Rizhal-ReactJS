351e51e5077b963810acd8b95523d54e51ba7de3
Izhal-29 <isal.informatika@gmail.com>
2022-08-06 20:44:23 +0800

    penambahan materi component

--- src/Materi/Komponen/index.jsx
+++ src/Materi/Komponen/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ClassComponent from "./pembahasan/ClassComponent";
-
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
 
 export default class Komponen extends React.Component{
   render(){
     return(
       <div>
-        <ClassComponent>
-          testing
-        </ClassComponent>
+        <ClassComponent nama="Rizhal"/>
+        {/* <ClassComponent></ClassComponent> */}
+        <FunctionalComponent nama="Rizhal"/>
       </div>
     )
   }
