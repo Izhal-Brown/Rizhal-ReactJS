2c3617ed0eb31b0397128db70a52600d3160b0cc
Izhal-Brown <informatika749@gmail.com>
2022-08-06 15:38:15 +0800

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
 import Komponen from './Materi/Komponen';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <Komponen>
         Ini Percobaan APLIKASI PERTAMA
       </Komponen>
