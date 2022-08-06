0e5374744c3267aa8ddb0ea674dfd266a6e8171a
Izhal-Brown <informatika749@gmail.com>
2022-08-06 15:44:31 +0800

    perubahan lagi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Komponen from './Materi/Komponen';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Komponen>
-        Ini Percobaan APLIKASI PERTAMA
-      </Komponen>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
